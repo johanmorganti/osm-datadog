dd5b0100f0cb9a89a95acc08fb284a18056feadd
Johan Morganti <johan.morganti@datadoghq.com>
2021-12-13 21:19:19 +0900

    Refactor code (add functions)

--- send-new-changesets.py
+++ send-new-changesets.py
@@ -1,6 +1,6 @@
 import requests
 import xml.etree.ElementTree as ET
-from os import path
+from os import kill, path
 import pickle
 import json
 from io import StringIO
@@ -13,56 +13,66 @@ from pprint import pprint
 
 FILENAME = 'previous_max_changeset.pk'
 
-# Defining the host is optional and defaults to https://api.datadoghq.com
-# DD_SITE, DD_API_KEY, DD_APP_KEY are supported by default :
-# https://github.com/DataDog/datadog-api-client-python/blob/master/src/datadog_api_client/v2/configuration.py
-dd_configuration = Configuration()
+def get_last_changesets():
+    ## Based on the latest version of OSM API : https://wiki.openstreetmap.org/wiki/API_v0.6
+    url = "https://api.openstreetmap.org/api/0.6/changesets"
 
-## Based on the latest version of OSM API : https://wiki.openstreetmap.org/wiki/API_v0.6
-url = "https://api.openstreetmap.org/api/0.6/changesets"
+    response = requests.get(url)
 
-response = requests.get(url)
+    xml = ET.fromstring(response.text)
 
-xml = ET.fromstring(response.text)
+    # Get the latest changeset IDs
+    # XML is in the form of 
+    """ 
+    <osm>
+    <changeset id="113928427" created_at="2021-11-18T06:17:42Z" open="false" comments_count="0" changes_count="6" closed_at="2021-11-18T06:17:44Z" min_lat="15.3384649" min_lon="-91.8697209" max_lat="15.3386183" max_lon="-91.8694203" uid="12026398" user="<redacted>">
+    <tag k="changesets_count" v="73"/>
+    [...] # More k,v tags
+    </changeset>
+    <changeset id="113928426" created_at="2021-11-18T06:17:42Z" open="false" comments_count="0" changes_count="11" closed_at="2021-11-18T06:17:43Z" min_lat="-23.6402734" min_lon="47.3068178" max_lat="-23.6387451" max_lon="47.3096663" uid="13571396" user="<redated>">
+    <tag k="changesets_count" v="2200"/>
+    [...] # More k,v tags
+    </changeset>
+    [...] # more changesets
+    </osm>
+    """
+    return xml
 
-# Get the latest changeset IDs
-# XML is in the form of 
-""" 
-<osm>
-<changeset id="113928427" created_at="2021-11-18T06:17:42Z" open="false" comments_count="0" changes_count="6" closed_at="2021-11-18T06:17:44Z" min_lat="15.3384649" min_lon="-91.8697209" max_lat="15.3386183" max_lon="-91.8694203" uid="12026398" user="<redacted>">
-  <tag k="changesets_count" v="73"/>
-  [...] # More k,v tags
-</changeset>
-<changeset id="113928426" created_at="2021-11-18T06:17:42Z" open="false" comments_count="0" changes_count="11" closed_at="2021-11-18T06:17:43Z" min_lat="-23.6402734" min_lon="47.3068178" max_lat="-23.6387451" max_lon="47.3096663" uid="13571396" user="<redated>">
-  <tag k="changesets_count" v="2200"/>
-  [...] # More k,v tags
-</changeset>
-[...] # more changesets
-</osm>
-"""
-latest_changeset = int(xml[0].attrib['id'])
-last_run_changeset = 0
+def send_logs(list_logs):
+    # Defining the host is optional and defaults to https://api.datadoghq.com
+    # DD_SITE, DD_API_KEY, DD_APP_KEY are supported by default :
+    # https://github.com/DataDog/datadog-api-client-python/blob/master/src/datadog_api_client/v2/configuration.py
+    dd_configuration = Configuration()
 
-# load the last changeset from the last run
-if path.exists(FILENAME):
-    with open(FILENAME, 'rb') as fi:
-        last_run_changeset = pickle.load(fi)
+    # Enter a context with an instance of the API client
+    with ApiClient(dd_configuration) as api_client:
+        # Create an instance of the API class
+        api_instance = logs_api.LogsApi(api_client)
 
-print("Number of changesets to load : " + str(latest_changeset - last_run_changeset))
+        try:
+            # Send logs
+            api_response = api_instance.submit_log(list_logs)
+            pprint(api_response)
+        except ApiException as e:
+            print("Exception when calling LogsApi->submit_log: %s\n" % e)
 
-# dump lastest changeset for next runs
-with open(FILENAME, 'wb') as fi:
-    pickle.dump(latest_changeset, fi)
-    
-# Enter a context with an instance of the API client
-with ApiClient(dd_configuration) as api_client:
-    # Create an instance of the API class
-    api_instance = logs_api.LogsApi(api_client)
+def main():
+    xml_last_cs = get_last_changesets()
+
+    latest_changeset = int(xml_last_cs[0].attrib['id'])
+
+    # load the last changeset from the last run
+    if path.exists(FILENAME):
+        last_run_cs_number = 0
+        with open(FILENAME, 'rb') as fi:
+            last_run_cs_number = pickle.load(fi)
+
+    print("Number of changesets to load : " + str(latest_changeset - last_run_cs_number))
 
     list_logs = []
 
-    for changeset in xml:
-        if int(changeset.attrib['id']) <= last_run_changeset:
+    for changeset in xml_last_cs:
+        if int(changeset.attrib['id']) <= last_run_cs_number:
             # print("Already processed. Skipping " + changeset.attrib["id"])
             continue
         
@@ -85,16 +95,13 @@ with ApiClient(dd_configuration) as api_client:
             )
         
         list_logs.append(new_log_item)
-    
-    body = HTTPLog(list_logs)
 
-    try:
-        # Send logs
-        api_response = api_instance.submit_log(list_logs)
-        pprint(api_response)
-    except ApiException as e:
-        print("Exception when calling LogsApi->submit_log: %s\n" % e)
+    send_logs(list_logs)
+        
+    # dump lastest changeset for next runs
+    with open(FILENAME, 'wb') as fi:
+        pickle.dump(latest_changeset, fi)
+        
 
-
-
-
+if __name__ == '__main__':
+    main()
